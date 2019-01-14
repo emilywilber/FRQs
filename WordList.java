ff585cb3f46a9a8d7c0089b295159d363ebb3359
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2019-01-14 12:48:52 -0500

    Add files via upload

---
+++ WordList.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+
+/**
+ * Write a description of class WordList here.
+ * 
+ * @author Emily Wilber
+ * @version Jan 14, 2019
+ */
+public class WordList
+{
+    private ArrayList<String> myList;
+    
+    public WordList(ArrayList<String> myList) {
+        this.myList = myList;
+    }
+    
+    public int numWordsOfLength(int len)
+    {
+        int count = 0;
+        for (int i = 0; i < myList.size(); i++) {
+            String word = myList.get(i);
+            if (word.length() == len) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void removeWordsOfLength(int len)
+    {
+        for (int i = 0; i < myList.size(); i++) {
+            String word = myList.get(i);
+            if (word.length() == len) {
+                myList.remove(i);
+            }
+        }
+    }
+    
+    public String toString() {
+        return myList.toString();
+    }
+}
