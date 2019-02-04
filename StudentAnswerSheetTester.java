82e67a9668beacb9ce427718cc312c122bd0990c
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2019-02-04 12:38:34 -0500

    Add files via upload

--- StudentAnswerSheetTester.java
+++ StudentAnswerSheetTester.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 /**
  * tests the thing
  *
@@ -10,9 +11,16 @@ public class StudentAnswerSheetTester
 {
     public static void main(String[] args)
     {
-        ArrayList<String> paulAns;
-        paulAns.add("A");
+        ArrayList<String> key = new ArrayList<String>
+                        (Arrays.asList(new String[] {"A", "B"}));
+                        
+        ArrayList<String> paulAns = new ArrayList<String>
+                        (Arrays.asList(new String[] {"A", "B"}));
+        
         StudentAnswerSheet paul = new StudentAnswerSheet(paulAns, "Paul");
+        System.out.println(paul.getName() + ": " + paul.getScore(key));
+        
+        
         TestResults T = new TestResults();
         
         
