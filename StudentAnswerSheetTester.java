b1104436c3409b01476b988eac00b69589086d0c
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2019-02-04 11:58:53 -0500

    Add files via upload

---
+++ StudentAnswerSheetTester.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * tests the thing
+ *
+ * @author Emily Wilber
+ * @version Jan 31, 2019
+ */
+public class StudentAnswerSheetTester
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> paulAns = {"A", "B"};
+        StudentAnswerSheet paul = new StudentAnswerSheet(paulAns, "Paul");
+        TestResults T = {paul};
+        
+    }
+}
